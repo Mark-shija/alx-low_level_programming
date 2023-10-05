317cd8c6d41924d44161a5f9f9a56fe5014299d5
Mark-shija <plusmaxtechtz@gmail.com>
2023-10-05 11:12:16 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * The function print strings on the std output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
