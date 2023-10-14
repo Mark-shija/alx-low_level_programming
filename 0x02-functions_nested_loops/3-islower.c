2b19a55ad9ab3d7af48e51694ec3cb79810377a0
Mark-shija <plusmaxtechtz@gmail.com>
2023-10-14 06:30:36 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _islower() - finds lower case a-z.
+ *
+ * return: Always 1.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		
+		return (1);
+	
+	return (0);
+}
