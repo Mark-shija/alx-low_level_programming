8fe759f4dd113db5614dbd25bb080bc441f1bd43
Mark-shija <plusmaxtechtz@gmail.com>
2023-10-17 23:54:57 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+#0x05. C - Pointers, arrays and strings
+#Published by: Maxius Emmanuel
+#Holberton School 
+#ALX Software Eng Program -2023
+ 
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ *  * File:main.h
+ *   * Auth: Maxius Emmanuel Shija
+ *    * Desc: Header file containing prototypes for all functions
+ *     *       used in the 0x05-pointers_arrays_strings directory.
+ *      */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
