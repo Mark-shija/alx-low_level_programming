92ba51b9c237162d29ec2b87178930b4555b030a
Mark-shija <plusmaxtechtz@gmail.com>
2023-10-19 04:44:45 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints string in reverse
+ * @s: function string to be printed
+ *
+ * return: reversed string
+ */
+void print_rev(char *s)
+{
+	int i;
+	int counter;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+
+	for (j = counter - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
