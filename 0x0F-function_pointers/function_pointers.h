3daabeda5f4aeac04976ca0cc6b5af7b8b687ccd
Mark-shija <plusmaxtechtz@gmail.com>
2023-11-10 04:57:48 -0800

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,9 @@
 #include <stddef.h>
+/**
+ * function_pointers_h - holds header file
+ *
+ * return: No any return
+ */
 #ifndef function_pointers_h
 #define function_pointers_h
 void print_name(char *name, void (*f)(char *));
