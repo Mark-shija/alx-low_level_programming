c400d20276ce86485c1a193b10eccb29b10a9fe6
Mark-shija <plusmaxtechtz@gmail.com>
2023-11-10 05:24:40 -0800

    2-int_index.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,8 +4,8 @@
  *
  * return: No any return
  */
-#ifndef function_pointers_h
-#define function_pointers_h
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTERS_H
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
