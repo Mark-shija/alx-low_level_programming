6b1d143e89e4d8092c21bc14fd84e4bb69ed3eab
Mark-shija <plusmaxtechtz@gmail.com>
2023-11-20 13:08:47 -0800

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - Count the number of elements in a linked list.
+ * @h: A pointer to the head of the linked list.
+ *
+ * Return: The number of elements in the linked list.
+ */
+size_t listint_len(const listint_t *h)
+{
+    size_t count = 0; 
+
+
+    while (h != NULL)
+    {
+        count++;      /* Increment the count for each node */
+        h = h->next;  /* points to the next node */
+    }
+
+    return count;
+}
+
