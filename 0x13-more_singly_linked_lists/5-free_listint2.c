82cb8fd5849b4e33f9d8006519a3765bde0b60c3
Mark-shija <plusmaxtechtz@gmail.com>
2023-11-21 13:54:44 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,11 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current;
+
 	while (*head != NULL)
 	{
 		current = *head;
 		*head = (*head)->next;
-	       	free(current);
+		free (current);
 	}
 }
