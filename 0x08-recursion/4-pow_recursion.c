0e6efc4ad32bd40de1a9a268dd2edc81414e501f
Mark-shija <plusmaxtechtz@gmail.com>
2024-01-11 13:24:08 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,6 @@
  * _pow_recursion - return the power of x raised by y.
  * @x: Number to raise.
  * @y: raising factor or power.
- * 
  * Return: always reult of power.
  */
 int _pow_recursion(int x, int y)
