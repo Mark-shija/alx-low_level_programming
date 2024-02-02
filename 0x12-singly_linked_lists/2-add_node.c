0b0daf6db7479a2d8fa291d4ce5b152e4aafa94d
Mark-shija <plusmaxtechtz@gmail.com>
2024-02-02 11:32:20 -0800

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,16 +13,20 @@ list_t *add_node(list_t **head, const char *str)
 	int count = 0;
 	list_t *new_node;
 	char  *duplicate;
+
 	new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
+
 	duplicate = strdup(str);
+
 	if (duplicate == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	
+
 	while (str[count] != '\0')
 	{
 		count++;
